8a8d38dfb6b196cdf3eafc27260906590ce7c1ef
Laeticia <laeticiaariana@gmail.com>
2023-09-25 23:40:48 +0400

    landing page finished:70% just keep going

--- Fproject/web_donuts/urls.py
+++ Fproject/web_donuts/urls.py
@@ -4,6 +4,6 @@ from . import views
 # one urlpattern per line
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('home/', views.landing),
-    # path('menu/', views.menu,name = 'menu')
+    path('home/', views.home),
+    path('menu/', views.menu,name = 'menu')
 ]
--- Fproject/web_donuts/views.py
+++ Fproject/web_donuts/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
+from itertools import groupby
 from django.http import HttpResponse # pass view information into the browser
+from .models import Donut
 
 # takes a request, returns a response
 def index(request):
     return HttpResponse("Hello, world. You're in the donut website.")
 # Create your views here.
 
-# from .models import Donut
 
+def home (request):
+    return render(request,'home.html',{})
 
-# def landing (request):
-#     return render(request,'landing.html',{})
 # def menu(request):
 #     donuts = Donut.objects.all()
 #     return render(request, 'menu.html', {'donuts': donuts})
+def menu(request):
+    donuts = Donut.objects.all().order_by('category', 'name')  # Retrieve all donuts and order them by category and name
+    donuts_by_category = {}
+    
+    for category, items in groupby(donuts, key=lambda x: x.category):
+        donuts_by_category[category] = list(items)
+
+    return render(request, 'menu.html', {'donuts_by_category': donuts_by_category})
