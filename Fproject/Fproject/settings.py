d2416845d17b19045b658048aecc4a01e65300a9
Laeticia <laeticiaariana@gmail.com>
2023-10-04 12:57:03 +0400

    deployment testing

--- Fproject/Fproject/settings.py
+++ Fproject/Fproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*dosjq(uve66p3h11f&&-+p3&)!hf*j*272vs1ux^6kcp&!fij'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
